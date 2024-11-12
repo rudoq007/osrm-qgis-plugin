c6df3efbb434ade561e2639c42c0880537603a0e
WanixRSGIS <62269969+rudoq007@users.noreply.github.com>
2024-11-13 09:04:34 +1000

    Update osrm.py

--- osrm.py
+++ osrm.py
@@ -11,135 +11,88 @@
         email                : matthieu.viry@cnrs.fr
  ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 """
-from qgis.core import *
+from qgis.core import QgsInterface
 from qgis.utils import iface
-from qgis.PyQt.QtGui import (
-    QTranslator, qVersion, QCoreApplication,
-    QObject, SIGNAL, Qt, pyqtSlot, QSettings
-    )
-from qgis.PyQt.QtGui import (
-    QAction, QIcon, QMessageBox,
-    )
-# Initialize Qt resources from file resources.py
-import resources
+from qgis.PyQt.QtCore import QTranslator, QCoreApplication, QSettings, pyqtSlot
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction, QMessageBox
+
 import os.path
-# Import the code for the dialog
+import resources
 from osrm_dialog import (
     OSRMDialog, OSRM_table_Dialog, OSRM_access_Dialog,
     OSRM_batch_route_Dialog, OSRM_DialogTSP
-    )
+)
 
 
-class OSRM(object):
+class OSRM:
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
         """Constructor.
-
+        
         :param iface: An interface instance that will be passed to this class
             which provides the hook by which you can manipulate the QGIS
             application at run time.
         :type iface: QgsInterface
         """
-        self.dlg = None
-        # Save reference to the QGIS interface
         self.iface = iface
         self.canvas = iface.mapCanvas()
-        # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
+        self.dlg = None
+
         # initialize locale
         locale = QSettings().value('locale/userLocale')[0:2]
         locale_path = os.path.join(
             self.plugin_dir,
             'i18n',
-            'OSRM_{}.qm'.format(locale))
+            f'OSRM_{locale}.qm')
 
         if os.path.exists(locale_path):
             self.translator = QTranslator()
             self.translator.load(locale_path)
-
-            if qVersion() > '4.3.3':
-                QCoreApplication.installTranslator(self.translator)
+            QCoreApplication.installTranslator(self.translator)
 
         # Declare instance attributes
         self.actions = []
-        self.menu = self.tr(u'&Routing with OSRM')
-
-        self.toolbar = self.iface.addToolBar(u'Routing with OSRM')
-        self.toolbar.setObjectName(u'Routing with OSRM')
-#        self.loadSettings()
-    # noinspection PyMethodMayBeStatic
+        self.menu = self.tr('&Routing with OSRM')
+        self.toolbar = self.iface.addToolBar('Routing with OSRM')
+        self.toolbar.setObjectName('Routing with OSRM')
 
     def tr(self, message):
         """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
+        
         :param message: String for translation.
-        :type message: str, QString
-
+        :type message: str
         :returns: Translated version of message.
-        :rtype: QString
+        :rtype: str
         """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('Routing with OSRM', message)
 
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
+    def add_action(self, icon_path, text, callback, enabled_flag=True, 
+                   add_to_menu=True, add_to_toolbar=True, status_tip=None, 
+                   whats_this=None, parent=None):
         """Add a toolbar icon to the toolbar.
 
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
+        :param icon_path: Path to the icon for this action. 
         :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
+        :param text: Text for menu items.
         :type text: str
-
         :param callback: Function to be called when the action is triggered.
         :type callback: function
-
         :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
         :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
+        :param add_to_menu: Flag for adding to the menu.
         :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
+        :param add_to_toolbar: Flag for adding to the toolbar.
         :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
+        :param status_tip: Text to show when mouse hovers over the action.
         :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
+        :param whats_this: Text to show in status bar when hovered.
+        :param parent: Parent widget for the new action.
         :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
+        :returns: The action created, also added to self.actions list.
         :rtype: QAction
         """
 
@@ -158,12 +111,9 @@ class OSRM(object):
             self.toolbar.addAction(action)
 
         if add_to_menu:
-            self.iface.addPluginToWebMenu(
-                self.menu,
-                action)
+            self.iface.addPluginToWebMenu(self.menu, action)
 
         self.actions.append(action)
-
         return action
 
     def initGui(self):
@@ -171,65 +121,50 @@ class OSRM(object):
 
         self.add_action(
             ':/plugins/OSRM/img/icon.png',
-            text=self.tr(u'Find a route with OSRM'),
+            text=self.tr('Find a route with OSRM'),
             callback=self.run_route,
             parent=self.iface.mainWindow())
 
         self.add_action(
             ':/plugins/OSRM/img/icon_table.png',
-            text=self.tr(u'Get a time matrix with OSRM'),
+            text=self.tr('Get a time matrix with OSRM'),
             callback=self.run_table,
             parent=self.iface.mainWindow())
 
         self.add_action(
             ':/plugins/OSRM/img/icon_access.png',
-            text=self.tr(u'Make accessibility isochrones with OSRM'),
+            text=self.tr('Make accessibility isochrones with OSRM'),
             callback=self.run_accessibility,
-            parent=self.iface.mainWindow(),
-            )
+            parent=self.iface.mainWindow())
 
         self.add_action(
             None,
-            text=self.tr(u'Solve the Traveling Salesman Problem with OSRM'),
+            text=self.tr('Solve the Traveling Salesman Problem with OSRM'),
             callback=self.run_tsp,
             parent=self.iface.mainWindow(),
-            add_to_toolbar=False,
-            )
+            add_to_toolbar=False)
 
         self.add_action(
             None,
-            text=self.tr(u'Export many routes from OSRM'),
+            text=self.tr('Export many routes from OSRM'),
             callback=self.run_batch_route,
             parent=self.iface.mainWindow(),
-            add_to_toolbar=False,
-            )  # ':/plugins/OSRM/img/icon_batchroute.png'
+            add_to_toolbar=False)
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
         for action in self.actions:
-            self.iface.removePluginWebMenu(
-                self.tr(u'&Routing with OSRM'),
-                action)
+            self.iface.removePluginWebMenu(self.tr('&Routing with OSRM'), action)
             self.iface.removeToolBarIcon(action)
-        # remove the toolbar
         del self.toolbar
 
     @pyqtSlot()
     def run_route(self):
         """Run the window to compute a single viaroute"""
-        self.dlg = OSRMDialog(iface)
-        QObject.connect(
-            self.dlg.originEmit,
-            SIGNAL("canvasClicked(const QgsPoint&, Qt::MouseButton)"),
-            self.dlg.store_origin)
-        QObject.connect(
-            self.dlg.intermediateEmit,
-            SIGNAL("canvasClicked(const QgsPoint&, Qt::MouseButton)"),
-            self.dlg.store_intermediate)
-        QObject.connect(
-            self.dlg.destinationEmit,
-            SIGNAL("canvasClicked(const QgsPoint&, Qt::MouseButton)"),
-            self.dlg.store_destination)
+        self.dlg = OSRMDialog(self.iface)
+        self.dlg.originEmit.canvasClicked.connect(self.dlg.store_origin)
+        self.dlg.intermediateEmit.canvasClicked.connect(self.dlg.store_intermediate)
+        self.dlg.destinationEmit.canvasClicked.connect(self.dlg.store_destination)
         self.dlg.pushButtonOrigin.clicked.connect(self.get_origin)
         self.dlg.pushButtonIntermediate.clicked.connect(self.get_intermediate)
         self.dlg.pushButtonDest.clicked.connect(self.get_destination)
@@ -239,51 +174,41 @@ class OSRM(object):
     @pyqtSlot()
     def run_batch_route(self):
         """Run the window to compute many viaroute"""
-        self.nb_done = 0
-        self.dlg = OSRM_batch_route_Dialog(iface)
+        self.dlg = OSRM_batch_route_Dialog(self.iface)
         self.dlg.pushButton_about.clicked.connect(self.dlg.print_about)
         self.dlg.show()
 
     @pyqtSlot()
     def run_table(self):
         """Run the window for the table function"""
-        self.dlg = OSRM_table_Dialog(iface)
+        self.dlg = OSRM_table_Dialog(self.iface)
         self.dlg.pushButton_about.clicked.connect(self.dlg.print_about)
         self.dlg.show()
 
     @pyqtSlot()
     def run_tsp(self):
-        """Run the window for making accessibility isochrones"""
-        self.dlg = OSRM_DialogTSP(iface)
+        """Run the window for solving the Traveling Salesman Problem"""
+        self.dlg = OSRM_DialogTSP(self.iface)
         self.dlg.pushButton_about.clicked.connect(self.dlg.print_about)
         self.dlg.show()
 
     @pyqtSlot()
     def run_accessibility(self):
         """Run the window for making accessibility isochrones"""
-        self.dlg = OSRM_access_Dialog(iface)
-        QObject.connect(
-            self.dlg.originEmit,
-            SIGNAL("canvasClicked(const QgsPoint&, Qt::MouseButton)"),
-            self.dlg.store_origin
-            )
-        QObject.connect(
-            self.dlg.intermediateEmit,
-            SIGNAL("canvasClicked(const QgsPoint&, Qt::MouseButton)"),
-            self.dlg.store_intermediate_acces
-            )
+        self.dlg = OSRM_access_Dialog(self.iface)
+        self.dlg.originEmit.canvasClicked.connect(self.dlg.store_origin)
+        self.dlg.intermediateEmit.canvasClicked.connect(self.dlg.store_intermediate_acces)
         self.dlg.pushButtonOrigin.clicked.connect(self.get_origin)
         self.dlg.pushButton_about.clicked.connect(self.dlg.print_about)
         self.dlg.toolButton_poly.clicked.connect(self.polycentric)
         self.dlg.show()
-#        self.dlg.exec_()
 
     @pyqtSlot()
     def polycentric(self):
         QMessageBox.information(
-                self.iface.mainWindow(), 'Info',
-                "Experimental :\n\nAdd other source points and compute "
-                "polycentric accessibility isochrones")
+            self.iface.mainWindow(), 'Info',
+            "Experimental:\n\nAdd other source points and compute "
+            "polycentric accessibility isochrones")
         self.get_intermediate()
 
     def get_origin(self):
@@ -294,8 +219,3 @@ class OSRM(object):
 
     def get_intermediate(self):
         self.canvas.setMapTool(self.dlg.intermediateEmit)
-
-# TODO :
-# - ensure that the MapToolEmitPoint is unset when the plugin window is closed
-# - write a function to ensure that the marker layer is in the top of the route
-#    layer (in TSP dialog)
